bf8e8e6e55a9ce9dc7b64affaeab2937b7a6ad5f
nkiboi <nelly.j.kiboi@aexp.com>
2020-02-27 18:18:16 +0000

    chore(release): 1.2.3

--- check-version.js
+++
@@ -1,8 +0,0 @@
-
-
-const exec = require('child_process').exec;
-const regex = /^(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?$/gm;
-const commitMessage = process.env.TRAVIS_COMMIT_MESSAGE;
-if (commitMessage != null && regex.test(commitMessage)){
-    child = exec('npm run lerna:publish').stderr.pipe(process.stderr);
-};
